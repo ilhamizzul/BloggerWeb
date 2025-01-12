88462850e2af0677fea88164e785a52c38ad3513
ilhamizzul <ilhamizzul@gmail.com>
2025-01-12 20:17:01 +0700

    adding connectionString and config to program.cs and migrate the database into sql server

--- Blogger.Web/Program.cs
+++ Blogger.Web/Program.cs
@@ -1,7 +1,16 @@
+using Blogger.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BloggerDbContext>(options  =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("BloggerDbConnectionString")
+        )
+    );
 
 var app = builder.Build();
 
