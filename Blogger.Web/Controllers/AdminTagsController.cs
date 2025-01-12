21b6f1e465e32739e4a2a7e5f97753fba623c781
ilhamizzul <ilhamizzul@gmail.com>
2025-01-12 21:47:31 +0700

    create POST function to save tags data to databases

--- Blogger.Web/Controllers/AdminTagsController.cs
+++ Blogger.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogger.Web.Data;
+using Blogger.Web.Models.Domain;
+using Blogger.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blogger.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggerDbContext _bloggerDbContext;
+        public AdminTagsController(BloggerDbContext bloggerDbContext)
+        {
+            _bloggerDbContext = bloggerDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddTagRequest request)
+        {
+            var tag = new Tag { 
+                Name = request.Name,
+                DisplayName = request.DisplayName 
+            };
+
+            _bloggerDbContext.Tags.Add(tag);
+            _bloggerDbContext.SaveChanges();
+            _bloggerDbContext.Dispose();
+            return View("Add");
         }
     }
 }
