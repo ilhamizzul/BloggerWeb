998c3ff452731b839f08ab27e309bde0bf8cb6c5
ilhamizzul <ilhamizzul@gmail.com>
2025-01-12 19:41:18 +0700

    adding Domain Model

---
+++ Blogger.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Blogger.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public required string Heading { get; set; }
+        public required string PageTitle { get; set; }
+        public required string Content { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? FeaturedImageUrl { get; set; }
+        public required string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public required string Author { get; set; }
+        public bool Visible { get; set; }
+
+        public ICollection<Tag>? Tags { get; set; }
+    }
+}
---
+++ Blogger.Web/Models/Domain/Tag.cs
@@ -0,0 +1,11 @@
+﻿namespace Blogger.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string DisplayName { get; set; }
+
+        public ICollection<BlogPost>? BlogPosts { get; set; }
+    }
+}
