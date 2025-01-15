5067ff22217fe361db104aadcf4c0e6d2e74daf2
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-01-15 11:11:13 +0700

    adding add blog post form content

--- Blogger.Web/Controllers/AdminBlogPostsController.cs
+++ Blogger.Web/Controllers/AdminBlogPostsController.cs
@@ -1,13 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogger.Web.Models.Domain;
+using Blogger.Web.Models.ViewModels;
+using Blogger.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Blogger.Web.Controllers
 {
     public class AdminBlogPostsController : Controller
     {
-        [HttpGet]
-        public IActionResult Add()
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository)
         {
-            return View();
+            this.tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            // get Tag from repository
+            var tags = await tagRepository.GetAllTagsAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.DisplayName, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest request)
+        {
+            var blog = new BlogPost
+            {
+                Heading = request.Heading,
+                PageTitle = request.PageTitle,
+                Content = request.Content,
+                ShortDescription = request.ShortDescription,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                UrlHandle = request.UrlHandle,
+                PublishedDate = request.PublishedDate,
+                Author = request.Author,
+                Visible = request.Visible,
+                //Tags = request.SelectedTag
+            };
+
+            return RedirectToAction("List", "AdminTags");
         }
     }
 }
