2dd64c804dae9a84460b61cb1ce1441d589dc353
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-01-16 14:53:41 +0700

    create initial api for images

---
+++ Blogger.Web/Controllers/ImagesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
