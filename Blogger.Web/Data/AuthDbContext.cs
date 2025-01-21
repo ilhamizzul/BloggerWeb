568937bd84918a99f0ac4c39488ffce61d98247c
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-01-21 10:44:10 +0700

    fixing authdbcontext and adding migration to create new authDB

--- Blogger.Web/Data/AuthDbContext.cs
+++ Blogger.Web/Data/AuthDbContext.cs
@@ -10,43 +10,49 @@ namespace Blogger.Web.Data
         {
         }
 
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings =>
+        warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning));
+            base.OnConfiguring(optionsBuilder);
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             // Seed Roles (User, Admin, SuperAdmin)
-
-            //var adminRoleId = "b10b9221-4a67-40c4-a2fe-7028a4e3457f";
-            var adminRoleId = Guid.NewGuid().ToString();
-            var superAdminRoleId = Guid.NewGuid().ToString();
-            var userRoleId = Guid.NewGuid().ToString();
+            var adminRoleId = "5562b051-0ff9-4913-a3b8-761ae98a893d";
+            var superAdminRoleId = "1b6e78b4-7ca4-4fee-8378-fb0ab2c984c4";
+            var userRoleId = "7227d8fa-903f-49dc-9fbc-b9ea47ef76fa";
 
             var roles = new List<IdentityRole> {
                 new IdentityRole {
                     Name="SuperAdmin",
-                    NormalizedName="SuperAdmin",
+                    NormalizedName="SuperAdmin".ToUpper(),
                     Id = superAdminRoleId,
                     ConcurrencyStamp = superAdminRoleId
                 },
                 new IdentityRole {
                     Name="Admin",
-                    NormalizedName="Admin",
+                    NormalizedName="Admin".ToUpper(),
                     Id = adminRoleId,
                     ConcurrencyStamp = adminRoleId
                 },
                 new IdentityRole {
                     Name="User",
-                    NormalizedName="User",
+                    NormalizedName="User".ToUpper(),
                     Id = userRoleId,
                     ConcurrencyStamp = userRoleId
                 }
             };
             modelBuilder.Entity<IdentityRole>().HasData(roles);
-            
+
+            var SuperAdminUserId = "20398aba-59e7-493e-9c92-7e1f51583630";
             // Seed SuperAdmin account
             var superAdminUser = new IdentityUser
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = SuperAdminUserId,
                 UserName = "superadmin@blogger.com",
                 Email = "superadmin@blogger.com",
                 NormalizedEmail = "superadmin@blogger.com".ToUpper(),
@@ -61,18 +67,18 @@ namespace Blogger.Web.Data
             {
                 new IdentityUserRole<string>
                 {
-                    UserId = superAdminUser.Id,
-                    RoleId = roles[0].Id
+                    RoleId = adminRoleId,
+                    UserId = SuperAdminUserId
                 },
                 new IdentityUserRole<string>
                 {
-                    UserId = superAdminUser.Id,
-                    RoleId = roles[1].Id
+                    RoleId = superAdminRoleId,
+                    UserId = SuperAdminUserId
                 },
                 new IdentityUserRole<string>
                 {
-                    UserId = superAdminUser.Id,
-                    RoleId = roles[2].Id
+                    RoleId = userRoleId,
+                    UserId = SuperAdminUserId
                 },
             };
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(superAdminRole);
